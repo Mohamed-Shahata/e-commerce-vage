c306b3fb0a2a16357401e3421a38263613dd78b7
mohamed <mohamedmrslan@gmail.com>
2025-02-12 01:08:40 +0200

    update

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(helmet());
 app.use(cookieParser());
 app.use(cors({
   origin: "http://localhost:3000",
-  methods: "GET,POST,PUT,DELETE",
+  methods: "GET,POST,PUT,PATCH,DELETE",
   credentials: true
 }));
 

42b6301733fb7d974bc8a07485d68ed7b51c29b9
mohamed <mohamedmrslan@gmail.com>
2025-02-12 02:57:47 +0200

    update

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(fileUpload({ useTempFiles: true }));
 app.use(helmet());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:5500'],
+  origin: 'http://localhost:3000',
   methods: "GET, POST, PUT, PATCH, DELETE",
   credentials: true
 }));

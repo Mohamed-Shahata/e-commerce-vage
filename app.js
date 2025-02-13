97c85bb92eacbbf00c744680e25f810f5617a953
mohamed <mohamedmrslan@gmail.com>
2025-02-13 16:45:27 +0200

    update

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(cors({
 
 // Handle Preflight Requests
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // السماح بكل الأصوات أو تحديد أكثر من واحد
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
